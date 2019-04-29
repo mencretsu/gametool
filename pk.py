f2e86e110a2df83126a4eb14e80b7668dde89167
Otomatis Gagal <43661631+mbest99@users.noreply.github.com>
2019-04-30 01:08:59 +0700

    Add files via upload

---
+++ pk.py
@@ -0,0 +1,96 @@
+## p.py - Lazymux v3.0
+# -*- coding: utf-8 -*-
+##
+import os
+import sys
+from time import sleep as timeout
+
+def main():
+        print "   [01] ZONK"
+        print "   [10] Exit the ZonkTool\n"
+        zonk = raw_input("PILIH > ")
+
+        if zonk == "1" or zonk == "01":
+                print "\n    [1]press"
+                zok = raw_input("PILIH > ")
+
+        if zok =="1":
+                print "\n [1]press"
+                zol = raw_input("pilih-> ")
+
+        if zol =="1":
+                print "\n [1]press"
+                zo = raw_input("pilih")
+
+        if zok =="1":
+                print "\n [1]press"
+                zol = raw_input("pilih-> ")
+
+        if zol =="1":
+                print "\n [1]press"
+                zo = raw_input("pilih")
+                
+        if zok =="1":
+                print "\n [1]press"
+                zol = raw_input("pilih-> ")
+
+        if zol =="1":
+                print "\n [1]press"
+                zo = raw_input("pilih")
+                
+        if zok =="1":
+                print "\n [1]press"
+                zol = raw_input("pilih-> ")
+
+        if zol =="1":
+                print "\n [1]press"
+                zo = raw_input("pilih")
+        if zok =="1":
+                print "\n [1]press"
+                zol = raw_input("pilih-> ")
+
+        if zol =="1":
+                print "\n [1]press"
+                zo = raw_input("pilih")
+                
+        if zok =="1":
+                print "\n [1]press"
+                zol = raw_input("pilih-> ")
+
+        if zol =="1":
+                print "\n [1]press"
+                zo = raw_input("pilih")
+                
+        if zok =="1":
+                print "\n [1]press"
+                zol = raw_input("pilih-> ")
+
+        if zol =="1":
+                print "\n [1]press"
+                zo = raw_input("pilih")
+        if zok =="1":
+                print "\n [1]press"
+                zol = raw_input("pilih-> ")
+
+        if zol =="1":
+                print "\n [1]press"
+                zo = raw_input("pilih")
+                
+        if zok =="1":
+                print "\n [1]press"
+                zol = raw_input("pilih-> ")
+
+        if zol =="1":
+                print "\n [1]press"
+                zo = raw_input("pilih")
+
+        elif zonk == "10":
+                sys.exit()
+
+        else:
+                print "\nERROR: Wrong Input"
+                timeout(2)
+                restart_program()
+
+if __name__ == "__main__":
+        main()
